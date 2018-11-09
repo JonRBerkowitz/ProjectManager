76cf25b917cceafc4f56f8ac5d6fb920a33eaeb4
Jonathan Berkowitz <jberkowitz@MacBook-Pro-2.local>
2018-11-09 15:05:00 -0500

    create barebones controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  get '/signup', to: 'users#new'
+  resources :users, only: [:show, :create, :index, :destroy]
+  resources :lists
 end
