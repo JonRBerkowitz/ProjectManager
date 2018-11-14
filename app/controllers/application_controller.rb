9523b0ae26745ff5738cf050555c77f14da18998
Jonathan Berkowitz <jberkowitz@MacBook-Pro-2.local>
2018-11-14 16:50:47 -0500

    add nested route for user lists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    User.find_by_id(params[:user_id])
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,13 @@
 class ListsController < ApplicationController
 
   def index
-    @lists = List.all
+    @lists = current_user.lists
+    @user = current_user
   end
 
   def new
     @list = List.new
+    @user = current_user
   end
 
 end
