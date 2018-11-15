8b9feb57ce0ca1ced98e6be1b3d7cd7b480bf931
Jonathan Berkowitz <jberkowitz@MacBook-Pro-2.local>
2018-11-15 12:39:45 -0500

    broke and fixed task func

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+before_action :current_user
 
   def index
     @lists = current_user.lists
@@ -12,18 +13,24 @@ class ListsController < ApplicationController
   end
 
   def create
+    @user = current_user
     @list = List.new(list_params)
-    if @list.save
-      redirect_to user_lists_path
-    else
-      render :new
-    end
+      if @list.save
+          redirect_to edit_user_list_path(@user, @list)
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @user = current_user
+    @list = List.find(params[:id])
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:title, :due_date, user_ids:[], tasks_attributes: [:name, :done])
+    params.require(:list).permit(:title, :due_date, :sub_type, user_ids:[], tasks_attributes: [:name, :done])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :create, :index, :destroy] do
     resources :lists
   end
+
+  resources :lists, only: [:edit]
 end
