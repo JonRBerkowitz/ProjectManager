870fb433068a079c72ef6afe07442fbbeecef565
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-01 04:20:09 +0000

    basic signup works with username only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.username = params[:user][:username]
-    @user.save
-
-    redirect_to @user
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
     @user = current_user
   end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :list_users
   has_many :lists, through: :list_users
   has_many :tasks, through: :lists
+
+  validates :username, :presence => true
+  validates :username, :uniqueness => true
 end
