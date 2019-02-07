a4475f3eb96fb30636eb11cfe75e7f848df022dd
JonRBerkowitz <jonrberkowitz@gmail.com>
2019-02-06 22:54:16 -0500

    add overdue scope to lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,9 @@ class List < ApplicationRecord
   has_many :list_users
   has_many :users, through: :list_users
   has_many :tasks
-
+  def self.overdue
+    where("due_date < ?", Time.now)
+  end
   accepts_nested_attributes_for :tasks
 
   validates :title, presence: true
