e9e7bc9fe6fd55d370b2a4b3417e0092f8ec2bef
JonRBerkowitz <jonrberkowitz@gmail.com>
2019-02-10 08:40:52 -0500

    validates email broke unique projects again

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
 
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_format_of :email, with: EMAIL_REGEX
+
 end
