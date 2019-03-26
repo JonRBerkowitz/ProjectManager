b0a6abca3cfb57322f9ae6138fccc899c390c066
JonRBerkowitz <jonrberkowitz@gmail.com>
2019-03-26 14:32:03 -0400

    rework add task ux

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -56,7 +56,8 @@ function editTask(obj) {
 	let id = $(obj).data("id");
 	currentTask = id;
 	let projectId = $(obj).data("project-id");
-
+	
+	$('.new-task-form').hide();
 	$(`.current-task`).show();
 	$(`.add-task`).show();
 	$(`.edit-task`).hide();
@@ -82,26 +83,58 @@ function hideEditTask(obj) {
 
 }
 
-function saveTask() {
-	let name = $(`.inner-edit-task-container[data-id=${currentTask}] .edit-task-name`).val();
+function saveTask(obj) {
+
+	if (currentTask) {
+
+	let name = $(`.inner-edit-task-container[data-id=${currentTask}] .edit-task-name`).val() || "New Task";
 	let assignee = $(`.inner-edit-task-container[data-id=${currentTask}] .assignee select`).val();
 	let date = $(`.inner-edit-task-container[data-id=${currentTask}] .date input`).val();
 	let assigneeName = ($(`.inner-edit-task-container[data-id=${currentTask}] .assignee select option:selected`).text());
 
-	formData = { name: name, user_id: assignee, due_date: date };
+	var formData = { name: name, user_id: assignee, due_date: date };
 
-	$.ajax({
-		type: "PATCH",
-		url: `/tasks/${currentTask}`,
-		data: formData,
-		success: function(data) {
-			$(`#${currentTask}edit-task-container`).hide();
-			$(`#${currentTask}task-container`).show();
-			$(`#${currentTask}inner-task-container h3`).html(`${name}`);
-			$(`#${currentTask}inner-task-container p`).html(`Assignee: ${assigneeName}`);
-			$(`#${currentTask}inner-task-container h4`).html(`Due: ${date}`);
+		$.ajax({
+			type: "PATCH",
+			url: `/tasks/${currentTask}`,
+			data: formData,
+			success: function(data) {
+				$(`#${currentTask}edit-task-container`).hide();
+				$(`#${currentTask}task-container`).show();
+				$(`#${currentTask}inner-task-container h3`).html(`${name}`);
+				$(`#${currentTask}inner-task-container p`).html(`Assignee: ${assigneeName}`);
+				$(`#${currentTask}inner-task-container h4`).html(`Due: ${date}`);
+			}
+		});
+	} else {
+		let id = parseInt(obj.id);
+
+		let name = $(`#${id}task-name`).val() || "New Task";
+
+		var formData = {
+			name: name,
+			due_date: $(`#${id}task-date`).val(),
+			user_id : $(`#${id}select-user select option:selected`).val(),
+			project_id: id
 		}
-	});
+		$.ajax({
+			type: "POST",
+			url: "/tasks",
+			data: formData,
+			success: function(data) {
+				var source = document.getElementById("new-task-template").innerHTML;
+				var template = Handlebars.compile(source);
+				var result = template(data);
+
+				$(`#${id}task-box`).append(result);
+				$('.note-box').hide();
+				$('.new-task-form').hide();
+				$('.edit-task').hide();
+				$(`.add-task`).show();
+				$(`.save-button`).hide();
+			}
+		});
+	}
 
 }
 
@@ -154,86 +187,68 @@ function retrieveComments(obj) {
 		});
 	}
 
-	function showTaskForm(obj) {
-		let id = parseInt(obj.id);
-		$(`#${id}new-task-form`).toggle();
-	}
+function showTaskForm(obj) {
+	let id = parseInt(obj.id);
 
-	function addTask(obj) {
-		if (event.code == "Enter") {
-		let id = parseInt(obj.id);
-		console.log($(`#${id}new-task-date`).val());
-		var formData = {
-			name: $(`#${id}task-name`).val(),
-			due_date: $(`#${id}task-date`).val(),
-			user_id : obj.querySelector('option:checked').value,
-			project_id: id
+	currentTask = 0;
+
+	$('.save-button').hide();
+	$('.add-task').show();
+	$('.new-task-form').hide();
+	$('.edit-task').hide();
+	$('.current-task').show();
+
+	$(`#${id}new-task-form`).toggle();
+	$(`#${id}add-task`).hide();
+	$(`#${id}save`).show();
+}
+
+function addProject() {
+	$.ajax({
+		type: "POST",
+		url: "/projects",
+		data: {name: "New Project"},
+		success: function(data) {
+			var source = document.getElementById("new-item-template").innerHTML;
+			var template = Handlebars.compile(source);
+			var result = template(data);
+
+			$('.grid-row').append(result);
+			$('.new-task-form').hide();
+			$('.project-name-input').hide();
+			$('.save-button').hide();
 		}
-		console.log(formData);
-		$.ajax({
-			type: "POST",
-			url: "/tasks",
-			data: formData,
-			success: function(data) {
-				var source = document.getElementById("new-task-template").innerHTML;
-				var template = Handlebars.compile(source);
-				var result = template(data);
+	});
+}
 
-				$(`#${id}task-box`).append(result);
-				$('.note-box').hide();
-				$('.new-task-form').hide();
-				$('.edit-task').hide();
-			}
-		});
-		}	
-	}
+function deleteTask(obj) {
+	let id = parseInt(obj.id);
+	console.log(id);
+	$.ajax({
+		type: "DELETE",
+		url: `/tasks/${id}`,
+		success: function() {
+			$(`#${id}task-container`).remove();
+		}
+	});
+}
 
-	function addProject() {
-		$.ajax({
-			type: "POST",
-			url: "/projects",
-			data: {name: "New Project"},
-			success: function(data) {
-				var source = document.getElementById("new-item-template").innerHTML;
-				var template = Handlebars.compile(source);
-				var result = template(data);
+function setTaskStatus(obj, done) {
+	let id = obj.getAttribute('data-id');
+	let status = { done: !done }
+	$.ajax({
+		type: "PATCH",
+		url: `/tasks/${id}`,
+		data: status,
+		success: function(data) {
+			console.log(data);
+			$(`#${id}task`).toggleClass('done');
+			obj.removeAttribute('onClick');
+			obj.setAttribute('onClick', `setTaskStatus(this, ${!done})`);
+		}
+	});
 
-				$('.grid-row').append(result);
-				$('.new-task-form').hide();
-				$('.project-name-input').hide();
-				$('.save-button').hide();
-			}
-		});
-	}
-
-	function deleteTask(obj) {
-		let id = parseInt(obj.id);
-		console.log(id);
-		$.ajax({
-			type: "DELETE",
-			url: `/tasks/${id}`,
-			success: function() {
-				$(`#${id}task-container`).remove();
-			}
-		});
-	}
-
-	function setTaskStatus(obj, done) {
-		let id = obj.getAttribute('data-id');
-		let status = { done: !done }
-		$.ajax({
-			type: "PATCH",
-			url: `/tasks/${id}`,
-			data: status,
-			success: function(data) {
-				console.log(data);
-				$(`#${id}task`).toggleClass('done');
-				obj.removeAttribute('onClick');
-				obj.setAttribute('onClick', `setTaskStatus(this, ${!done})`);
-			}
-		});
-
-	}
+}
 
 
 //Project Sort
